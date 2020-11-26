895afc935bf6f50b569b5359198852afe65fd1ca
Evelyn Hasama <ehasama11@gmail.com>
2020-11-25 21:17:46 -0800

    Initial Commit

---
+++ DonateIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DonateIt
+//
+//  Created by Evelyn Hasama on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
